3516bb3c5e23c8d6c91b82ceafd262f5250cb69a
valstan <valstan@valstan.ru>
2022-10-15 01:14:28 +0300

    Включил дебаг фласка

--- local.py
+++ local.py
@@ -1,13 +1,23 @@
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
-res = requests.post("http://ovz3.id45d.pq4yn.vps.myjino.ru:49225/api/nagrada",
-                    json={"n_key": "post_field",
-                          "collection": "vita",
-                          "table": "mashina",
-                          "body": {"mashine_name": "Лада", "kolesa": 5}})
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+# session.mount('https://', adapter)
 
-print(res)
-try:
-    print(res.json())
-except Exception as exc:
-    print("Неверная строка запроса - ", exc)
+session.post("http://ovz3.id45d.pq4yn.vps.myjino.ru:49233/api/nagrada")
+
+# res = requests.post("http://ovz3.id45d.pq4yn.vps.myjino.ru:49233/api/nagrada",
+#                     json={"n_key": "post_field",
+#                           "collection": "vita",
+#                           "table": "mashina",
+#                           "body": {"mashine_name": "Лада", "kolesa": 5}})
+
+# print(res)
+# try:
+#     print(res.json())
+# except Exception as exc:
+#     print("Неверная строка запроса - ", exc)
